c222dcdec8271b4c9171bc29b9d1e8a34fc73536
Shivansh10000 <shivanshj157@gmail.com>
2023-05-29 11:04:41 +0530

    connection commit

--- src/App.js
+++ src/App.js
@@ -9,46 +9,56 @@ function App() {
   const [listOfFriends, setListOfFriends] = useState([])
 
   const addFriend = () => {
-    Axios.post('http://localhost:3001/addFriend', {name:name, age:age}).then((response)=> {
-      setListOfFriends([...listOfFriends, {_id:response.data._id, name: name, age: age}])
-      alert("New Friend added !")
-    }).catch(() => {
-      alert("Unsuccessful")
-    })
+    Axios.post('https://friendappbackend.onrender.com/addFriend', {name: name, age: age})
+      .then((response)=> {
+        setListOfFriends([...listOfFriends, {_id: response.data._id, name: name, age: age}])
+        alert("New Friend added!")
+      })
+      .catch(() => {
+        alert("Unsuccessful")
+      })
   }
 
   const updateFriend = (id) => {
     const newAge = prompt("Enter new age : ");
 
-    Axios.put('http://localhost:3001/update', {newAge: newAge, id: id}).then(()=>{
-      setListOfFriends(listOfFriends.map((val) => {
-        return val._id === id ? {_id: id, name: val.name, age: newAge} : val
-      }))
-    })
+    Axios.put('https://friendappbackend.onrender.com/update', {newAge: newAge, id: id})
+      .then(() => {
+        setListOfFriends(listOfFriends.map((val) => {
+          return val._id === id ? {_id: id, name: val.name, age: newAge} : val
+        }))
+      })
+      .catch(() => {
+        alert("Unsuccessful")
+      })
   }
 
   const deleteFriend = (id) => {
-    Axios.delete(`http://localhost:3001/delete/${id}`).then(()=>{
-      setListOfFriends(listOfFriends.filter((val) => {
-        return val._id !== id
-      }))
-    })
+    Axios.delete(`https://friendappbackend.onrender.com/delete/${id}`)
+      .then(() => {
+        setListOfFriends(listOfFriends.filter((val) => {
+          return val._id !== id
+        }))
+      })
+      .catch(() => {
+        alert("Unsuccessful")
+      })
   }
 
   useEffect(() => {
-    Axios.get('http://localhost:3001/read').then((response)=> {
-      // console.log(response);
-      setListOfFriends(response.data)
-
-    }).catch(() => {
-      alert("Unsuccessful")
-    })
+    Axios.get('https://friendappbackend.onrender.com/read')
+      .then((response) => {
+        setListOfFriends(response.data)
+      })
+      .catch(() => {
+        alert("Unsuccessful")
+      })
   }, [])
 
   return (
     <div className="App">
       <div className="inputs">
-        <h3>Enter the name : </h3>
+        <h3>Enter the name:</h3>
         <input 
           type="text" 
           className="input" 
@@ -56,7 +66,7 @@ function App() {
           onChange={(event) => {setName(event.target.value)}}
         />
 
-        <h3>Enter the age :</h3>
+        <h3>Enter the age:</h3>
         <input 
           type="number" 
           className="input" 
@@ -65,7 +75,6 @@ function App() {
         />
 
         <button onClick={addFriend}>Add Friend</button>
-
       </div>
 
       <h2>List of friends</h2>
@@ -75,20 +84,15 @@ function App() {
           return(
             <div className='friendContainer'>
               <div className='friend'>
-              <h3>Friend Name : {val.name}</h3>
-              <h3>Friend Age : {val.age} </h3>
+                <h3>Friend Name: {val.name}</h3>
+                <h3>Friend Age: {val.age}</h3>
               </div>
-
               <button onClick={() => {updateFriend(val._id)}}>Update</button>
               <button onClick={() => {deleteFriend(val._id)}}>X</button>
-
             </div>
           )
         })}
       </div>
-
-      
-
     </div>
   );
 }
